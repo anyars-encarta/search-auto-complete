b1b162eb7cfafc56a36f3e3b0af9e98671a17956
anyars-encarta <anyarsencarta@gmail.com>
2024-02-19 19:01:15 +0000

    Implement auto-complete search

---
+++ src/components/search-auto-complete/index.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+const SearchAutoComplete = () => {
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [searchParam, setSearchParam] = useState('');
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch('https://dummyjson.com/users');
+            const data = await response.json();
+            
+            if (data && data.users && data.users.length) {
+                setUsers(data.users.map(userItem => userItem.firstName));
+                setLoading(false);
+                setErrorMessage(null);
+            };
+        } catch (e) {
+            setLoading(false);
+            setErrorMessage(e.message);
+        };
+    };
+
+    console.log(users);
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    if (loading) {
+        return <h3>Loading users. Please wait...</h3>
+    };
+
+    const handleSearchUsers = (e) => {
+        const query = e.target.value.toLowerCase()
+        setSearchParam(query);
+
+        if (query > 1) {
+          const filteredData = users && users.length ?
+          users.filter(item => item.toLowerCase).indexOf(query) > -1
+          : []
+          setFilteredUsers(filteredData);
+          setShowDropdown(true);
+        } else {
+            setShowDropdown(false);
+        }
+    }
+
+    console.log(users, filteredUsers);
+
+    return (
+        <div className="search-auto-complete-container">
+            <input
+              value={searchParam} 
+              name="search-users" 
+              type="text" 
+              placeholder="Search Users here..."
+              onChange={handleSearchUsers}
+            />
+        </div>
+    );
+};
+
+export default SearchAutoComplete;
